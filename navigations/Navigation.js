4abf63bddaf52d50820fb8469a1d1b11d6ccf032
Renatogranados <58044053+Renatogranados@users.noreply.github.com>
2021-04-01 11:17:38 +0100

    Agregando íconos al bottom

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -2,26 +2,68 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-elements';
 
 // Modulos propios
-// import Favorites from '../screens/Favorites';
-// import TopRestaurants from '../screens/TopRestaurants';
-// import Search from '../screens/Search';
-// import Account from '../screens/Account';
 import RestaurantsStack from './RestaurantsStack';
 import AccountStack from './AccountStack';
 import TopRestaurantsStack from './TopRestaurantsStack';
 import SearchStack from './SearchStack';
 import FavoritesStack from './FavoritesStack';
 
+
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+    const screenOptions = (route, color) => {
+        let iconName;
+        switch(route.name){
+            case "restaurants":
+                iconName = "compass-outline"
+                break;
+
+            case "favorites":
+                iconName = "heart-outline"
+                break;
+
+            case "top-restaurants":
+                iconName = "star-outline"
+                break;
+
+            case "search":
+                iconName = "magnify"
+                break;
+
+            case "account":
+                iconName = "home-outline"
+                break;
+            }
+
+        return (
+            <Icon
+                type="material-community"
+                name={iconName}
+                size={22}
+                color={color}
+            />
+        )
+        }
+    
+
    return (
       <NavigationContainer>
-         <Tab.Navigator>
+          {/* Por defecto, la pantalla será el de restaurantes */}
+         <Tab.Navigator
+            initialRouteName="restaurants"
+            tabBarOptions={{
+                inactiveTintColor: "#5c5c60",
+                activeTintColor: "#31b44c"
+            }}
+            screenOptions={({route}) => ({
+                tabBarIcon: ({color}) => screenOptions(route, color)
+            })}
+         >
             {/* Aquí vamos a crear los botones inferiores y el stack superior */}
-
 
              {/* Botón para los restaurantes: */}
              <Tab.Screen
@@ -43,19 +85,16 @@ export default function Navigation() {
              ></Tab.Screen>
             {/* Botón para los Buscar: */}
              <Tab.Screen
-                name="buscar"
+                name="search"
                 component={SearchStack}
                 options={{title: "Buscar"}}
              ></Tab.Screen>
             {/* Botón para los Cuenta: */}
              <Tab.Screen
-                name="cuenta"
+                name="account"
                 component={AccountStack}
                 options={{title: "Cuenta"}}
              ></Tab.Screen>
-
-
-
          </Tab.Navigator>
       </NavigationContainer>
    );
